c242b1d25b8a6ea8a05eb01360be09f1cf608188
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-08-15 00:52:07 +0200

    Compilation using a DLL of filib++.

--- tests/reuse.ml
+++ tests/reuse.ml
@@ -6,10 +6,12 @@ let sin1 = Filib.(sin 1.)
 
 let () =
   Filib.(
-    let x = sin 1. +. cos 1. in (* requires 2 temp vars *)
-    let y = cos 1. in
-    (* If the temporary variable containing the value of x is reused
-       for y, the value of x will be modified. *)
+    let x = sin 1. +. cos 1. in (* requires 2 temp vars, one can be
+                                   used for the final result. *)
+    let y = cos 1. in (* could reuse the temp var which was not used
+                         for holding [x]. *)
+    (* CHECK: If the temporary variable containing the value of x is
+       (wrongly) reused for y, the value of x will be modified. *)
     if x = sin1 then printf "test passed.\n"
     else printf "test failed.\n"
   )
